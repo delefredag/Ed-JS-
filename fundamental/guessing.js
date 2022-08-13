24173d5d61c32867a4c7952825046ab41d1e2741
delefredag <fbmlife0909@gmail.com>
2022-08-13 22:38:41 +0900

    Sat

---
+++ fundamental/guessing.js
@@ -0,0 +1,14 @@
+// what are you planinng to do, lay it down ! (highly recommended) 
+
+// Generate a random number
+// Give the user the abillity to guess
+// If they guess and they are wrong, ask them again (hint);
+// If they win - say that they won 
+
+
+function guessGame(){
+    let randomNr = Math.random();
+    console.log(randomNr);
+}
+
+guessGame();
